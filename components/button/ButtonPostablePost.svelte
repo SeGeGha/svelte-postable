1767723feec99120408288d7fb11b487d540cc51
Sergey Sudakov <s3gaddtv@gmail.com>
2021-02-13 20:10:06 +0300

    feat: create buttonPostablePost

---
+++ components/button/ButtonPostablePost.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { LOADING_STATUSES } from '../../utils/postable_list';
+
+	export let plist;
+	export let data;
+	export let uri = null;
+	export let operation = 'post';
+	export let confirmBtnTitle = '';
+	export let className = '';
+
+	function run_action() {
+		plist[operation]( data, uri );
+	}
+</script>
+
+<button 
+	type="button" 
+	title={confirmBtnTitle} 
+	class={`pbtn ${className}`} 
+	disabled={LOADING_STATUSES.indexOf($plist.meta.status)!==-1} 
+	on:click={run_action}
+>
+	<slot>X</slot>
+</button>
